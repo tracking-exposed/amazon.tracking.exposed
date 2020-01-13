d01a23831cad1ca21dc757486fae6b5059ff70b7
Claudio <claudio@tracking.exposed>
2020-01-13 09:43:43 +0100

    progress with experiments approach model

---
+++ content/experiments/_index.md
@@ -0,0 +1,13 @@
+---
+title: Experiment list
+date: 2019-07-22T15:01:21+01:00
+draft: false
+description: Experiment list, tools for researcher doing algorithm analysis
+
+og_title: "ytTREX experiment list"
+og_type: "website"
+og_image: "http://youtube.tracking.exposed/yttrex-logo.jpg"
+og_url: "https://youtube.tracking.exposed/experiments"
+og_description: "youtube.tracking.exposed experiment list"
+---
+
---
+++ static/js/experiments.js
@@ -0,0 +1,62 @@
+
+async function activate() {
+    console.log("activation begin: fetching all the HREFs");
+    const step1 = _.first(window.location.href.split("/#"))
+    const testName = _.last(step1.split('/'));
+    const activadat = _.map($(".experiment"), function(node) {
+        return {
+            id: node.getAttribute('id'),
+            href: node.getAttribute('link'),
+            test: testName
+        };
+    })
+    console.log(activadat);
+
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(activadat),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    const r = await fetch(buildApiUrl('experiments', 'register', 2), options)
+        .then(res => res.json())
+        .then(res => console.log(res));
+
+    console.log("Activation: ", r);
+}
+
+function loadActivation() {
+    console.log("Load activation sections");
+    $("#actions").text("Experiment activation");
+    $("#actions + p").text("This is the sequence you'll make repeat to all the test participants:");
+    $(".experiment").addClass('experiment-muted');
+    $("#activation-form").style({display: show });
+    $("#activation-button").click(activate);
+}
+
+
+
+function loadAccess() {
+    console.log("Loading: access sections");
+    $("#actions").text("Action: You want to participate in the experiment");
+    $("#actions + p").text("- actions TODO -");
+}
+
+function dynamicTestPage() {
+    const chunks = window.location.href.split("/#");
+    console.log(chunks);
+    if(_.size(chunks) == 2 && _.last(chunks) == 'activate') {
+        loadActivation();
+    }
+    else if(_.size(chunks) == 2 && _.last(chunks) == 'access') {
+        loadAccess();
+    } else {
+        $("#actions").hide();
+        $("#experiments").hide();
+
+        $("#conclusion").hide();
+        $("#conclusion + p").hide();
+    }
+};
